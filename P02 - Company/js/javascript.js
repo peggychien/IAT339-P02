0c2698197b5a94fb94c65fd39cf9d83e1ded4581
Darya <dtyugay@sfu.ca>
2019-06-27 14:35:06 -0700

    Added more media queries

--- P02 - Company/js/javascript.js
+++ P02 - Company/js/javascript.js
@@ -14,7 +14,7 @@ function carousel() {
 }
 
 
-// --------------------- faq accordion --------------------- 
+// --------------------- faq accordion ---------------------
 
 var acc = document.getElementsByClassName("faqaccordion");
 var i;
@@ -27,6 +27,39 @@ for (i = 0; i < acc.length; i++) {
       answer.style.maxHeight = null;
     } else {
       answer.style.maxHeight = answer.scrollHeight + "px";
-    } 
+    }
   });
+}
+
+
+// ----------------------- HAMBURGER ------------------
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+// ------------------------ SCROLL TO TOP button
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
