bd6cb6643e70b5d289c1107563c6eb346a8207b2
Darya <dtyugay@sfu.ca>
2019-07-02 19:17:55 -0700

    Created a separate Javascript file for button and FAQ accordion + Added a white overlay when hover over image

---
+++ P02 - Company/js/faq.js
@@ -0,0 +1,16 @@
+// Accodion SCROLLING
+
+var acc = document.getElementsByClassName("faqaccordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.maxHeight){
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+}
